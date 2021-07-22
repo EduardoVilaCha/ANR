7780da02597e349adfc3e192d7b89f2cfe6ece1b
EduardoVilaCha <edu271999@gmail.com>
2021-07-22 22:27:08 +0100

    insecure

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = (os.path.join('static'),)
-STATIC_ROOT = ''
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
