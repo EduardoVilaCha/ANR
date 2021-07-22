80de5dcfbc540e8caee3848d5b1ca17ec158072e
EduardoVilaCha <edu271999@gmail.com>
2021-07-22 22:23:57 +0100

    Static Files

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = (os.path.join('static'),)
+STATIC_ROOT = ''
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
