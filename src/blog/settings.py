078c1bd0c04d723755ac0a8a26080574b8c12099
EduardoVilaCha <edu271999@gmail.com>
2021-07-22 22:12:17 +0100

    Static change

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -132,12 +132,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_in_env'),
+    '/action_not_reaction/src/static/',
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
