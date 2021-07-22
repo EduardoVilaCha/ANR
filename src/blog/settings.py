bfd2361e9e715b3464871f0722a83854ea92aef1
EduardoVilaCha <edu271999@gmail.com>
2021-07-22 15:19:36 +0100

    Debug

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y%i*u4r3y^gc_^8zt3p5m@mc0o58$u8_u_vgwaq6r$^5xj@oe-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['142.93.236.70', 'actionotreaction.com']
 
