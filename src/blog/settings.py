920ca27703fd4f9735546b035d09e790469e7b99
EduardoVilaCha <edu271999@gmail.com>
2021-07-23 10:36:24 +0100

    Pywin

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -1,17 +1,14 @@
+from django.core.management.utils import get_random_secret_key
+from pathlib import Path
 import os
 import sys
 import dj_database_url
 
-from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-from django.core.management.utils import get_random_secret_key
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
