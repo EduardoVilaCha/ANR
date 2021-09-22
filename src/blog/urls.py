9b3bac0e0da8f19ad8feb55925c0dcaa4f8c0c80
EduardoVilaCha <edu271999@gmail.com>
2021-09-22 11:45:02 +0200

    url

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,31 +3,21 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from posts.views import (
-    index,
-    blog,
-    post,
-    search,
-    post_create,
-    post_update,
-    post_delete
-)
+from posts.views import index, blog, post, search, post_create, post_update, post_delete
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index),
-    path('blog/', blog, name='post-list'),
-    path('search/', search, name='search'),
-    path('create/', post_create, name='post-create'),
-    path('post/<id>/', post, name='post-detail'),
-    path('post/<id>/update/', post_update, name='post-update'),
-    path('post/<id>/delete/', post_delete, name='post-delete'),
-    path('tinymce/', include('tinymce.urls')),
-    path('accounts/', include('allauth.urls')),
+    path("admin/", admin.site.urls),
+    path("", index),
+    path("blog/", blog, name="post-list"),
+    path("search/", search, name="search"),
+    path("create/", post_create, name="post-create"),
+    path("post/<slug:post>/", post, name="post-detail"),
+    path("post/<id>/update/", post_update, name="post-update"),
+    path("post/<id>/delete/", post_delete, name="post-delete"),
+    path("tinymce/", include("tinymce.urls")),
+    path("accounts/", include("allauth.urls")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
