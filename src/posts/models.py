97a5729ebb5f751673c4b72556a7bbbe5dd0c232
EduardoVilaCha <edu271999@gmail.com>
2021-09-25 11:03:55 +0200

    404 fixed

--- src/posts/models.py
+++ src/posts/models.py
@@ -40,7 +40,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    content = HTMLField("Content")
+    content = HTMLField()
     slug = models.SlugField(max_length=250, null=True, blank=True)
     # slug field !!!maybe change
 
