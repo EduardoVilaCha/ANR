fe12eee12ccd870dfde28106d015bba954a9975e
EduardoVilaCha <edu271999@gmail.com>
2021-09-27 09:41:51 +0200

    CKEDITOR_BASEPATH

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = "/media/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_in_env")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-CKEDITOR_BASEPATH = "/blog/src//static/ckeditor/ckeditor/"
+CKEDITOR_BASEPATH = "/blog/src/static/ckeditor/ckeditor/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
