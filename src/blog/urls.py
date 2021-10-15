8c39f00367f4f8a70177b766751bbba2035b4f6f
EduardoVilaCha <edu271999@gmail.com>
2021-10-15 19:04:01 +0200

    Ck editor 4 -> normal

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [
-        path("ckeditor5/", include("django_ckeditor_5.urls")),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += [
+    #     path("ckeditor5/", include("django_ckeditor_5.urls")),
+    # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
